1565ea0ded6e6a7af93d32f2b83d54ae88e8893f
Richard Turner <rclt201@gmail.com>
2019-09-24 10:45:51 +0100

    added drag handle

--- src/task.js
+++ src/task.js
@@ -8,7 +8,16 @@ border: 1px solid lightgrey;
 border-radius: 2px
 padding: 8px
 margin-bottom: 8px
-background-color: ${props => (props.isDragging ? 'lightgreen': 'white')}
+background-color: ${props => (props.isDragging ? 'lightgreen' : 'white')}
+display: flex;
+`
+
+const Handle = styled.div`
+  width: 20px;
+  height: 20px;
+  background-color: orange;
+  border-radius: 4px;
+  margin-right: 8px;
 `
 
 export default class Column extends React.Component {
@@ -18,10 +27,10 @@ export default class Column extends React.Component {
         {(provided, snapshot) => (
           <Container
             {...provided.draggableProps}
-            {...provided.dragHandleProps}
             ref={provided.innerRef}
             isDragging={snapshot.isDragging}
           >
+            <Handle {...provided.dragHandleProps} />
             {this.props.task.content}
           </Container>
         )}
