3794dc3ef4f4e14243ceb3853c7ef4e0f4fef8be
Richard Turner <rclt201@gmail.com>
2019-09-24 10:40:57 +0100

    added colors during drag

--- src/app.js
+++ src/app.js
@@ -9,7 +9,22 @@ import Column from './column'
 class App extends React.Component {
   state = initialData
 
+  onDragStart = () => {
+    document.body.style.color = 'orange'
+    document.body.style.transition = 'background-color 0.2s ease'
+  }
+
+  onDragUpdate = update => {
+    const { destination } = update
+    const opacity = destination
+      ? destination.index / Object.keys(this.state.tasks).length
+      : 0
+    document.body.style.backgroundColor = `rgba(153, 141, 217, ${opacity})`
+  }
+
   onDragEnd = result => {
+    document.body.style.color = 'inherit'
+    document.body.style.backgroundColor = 'inherit'
     const { destination, source, draggableId } = result
     if (!destination) return
 
@@ -42,7 +57,11 @@ class App extends React.Component {
 
   render() {
     return (
-      <DragDropContext onDragEnd={this.onDragEnd}>
+      <DragDropContext
+        onDragEnd={this.onDragEnd}
+        onDragStart={this.onDragStart}
+        onDragUpdate={this.onDragUpdate}
+      >
         {this.state.columnOrder.map(columnId => {
           const column = this.state.columns[columnId]
           const tasks = column.taskIds.map(taskId => this.state.tasks[taskId])
